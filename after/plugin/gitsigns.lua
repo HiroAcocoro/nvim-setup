0914655f49c42687f79e5a576584bee490c3f0f5
Hiroshi Acocoro <57206370+HiroAcocoro@users.noreply.github.com>
2024-04-20 10:24:30 +0800

    feat(keymap): gitsigns additional keymaps

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,9 @@
-require('gitsigns').setup({})
+require('gitsigns').setup({
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
+        vim.keymap.set('n', '<leader>ht', gitsigns.toggle_current_line_blame)
+
+end
+})
