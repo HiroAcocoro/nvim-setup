9265ff34d83d1cbb9a7694d23d8885254e2f8e81
Hiroshi Acocoro <57206370+HiroAcocoro@users.noreply.github.com>
2024-04-20 10:38:07 +0800

    refactor(TSconfig): simplified config

--- lua/hiroshi/treesitter.lua
+++ lua/hiroshi/treesitter.lua
@@ -4,26 +4,7 @@ if not status_ok then
 end
 
 configs.setup({
-    ensure_installed = "maintained", -- one of "all" or a list of languages
-    custom_captures = {
-		["punctuation.bracket"] = "",
-		["constructor"]         = "",
-	},
-	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "yaml" } },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    },
+    ensure_installed = {"lua", "javascript", "typescript", "go"},
+    highlight = { enable = true },
+    indent = { enable = true },
 })
