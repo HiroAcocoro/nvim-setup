cc4c23991bea4b4b2ea4f547a1965d8076294e53
Hiroshi Acocoro <57206370+HiroAcocoro@users.noreply.github.com>
2024-04-21 03:33:35 +0800

    feat(plugin): lsp initial configs WIP

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,27 @@
 return {
-    'nvim-telescope/telescope.nvim',
+  {'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
     dependencies = { { 'nvim-lua/plenary.nvim' }, },
     config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") });
-        end)
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>ps', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") });
+      end)
     end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extenstions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown{}
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
