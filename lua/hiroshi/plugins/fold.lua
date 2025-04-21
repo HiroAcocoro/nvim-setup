9f32baeb1b11452f0026d8e16a3a8aa4513c9169
HiroAcocoro <hiroshicacocoro@gmail.com>
2025-04-21 13:10:50 -0600

    feat: fold plugin setup

--- lua/hiroshi/plugins/fold.lua
+++ lua/hiroshi/plugins/fold.lua
@@ -3,6 +3,9 @@ return {
 	dependencies = { "kevinhwang91/promise-async" },
 	opts = {
 		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+		provider_selector = function(bufnr, filetype, buftype)
+			return { "lsp", "indent" }
+		end,
 	},
 	config = function(_, opts)
 		vim.api.nvim_create_autocmd("FileType", {
@@ -13,7 +16,20 @@ return {
 			end,
 		})
 
-		vim.opt.foldlevelstart = 99
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+		vim.keymap.set("n", "zK", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				vim.lsp.buf.hover()
+			end
+		end, { desc = "Peek Fold" })
+
 		require("ufo").setup(opts)
 	end,
 }
